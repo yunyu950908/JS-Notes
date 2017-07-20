ccdbd5e90594dde730e8c72630cf2ca0312f63ed
yunyu950908 <yunyu950908@gmail.com>
2017-07-20 09:43:52 +0800

    AJAX&mock-server

---
+++ advance/task12/.router.js
@@ -0,0 +1,33 @@
+function setRouter(app){ 
+ var router = app; 
+
+//V1
+
+router.get('/getInfoV1', function (req, res) {
+    var nextIndex = req.query.index,
+        len = req.query.length,
+        data = []
+    for (var i = 0; i < len; i++) {
+        data.push("内容" + (parseInt(nextIndex) + i))
+    }
+    console.log(nextIndex, data)
+    res.send(data)
+})
+
+
+//V2
+
+router.get('/getInfoV2', function (req, res) {
+    var nextIndex = req.query.index,
+        len = req.query.length,
+        data = []
+    for (var i = 0; i < len; i++) {
+        data.push("内容" + (parseInt(nextIndex) + i))
+    }
+    setTimeout(function () {
+        res.send(data)
+    }, 1000)
+})
+
+}
+ module.exports.setRouter = setRouter
