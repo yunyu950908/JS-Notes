ea5bd24cfdb4d545fd0a52575bcf8258f12f93f7
yunyu950908 <yunyu950908@gmail.com>
2017-07-20 20:38:17 +0800

    ajax封装

--- advance/task12/router.js
+++ advance/task12/router.js
@@ -27,3 +27,17 @@ router.get('/getInfoV2', function (req, res) {
     }, 1000)
 })
 
+
+//
+router.get('/getInfo', function (req, res) {
+    var nextIndex = req.query.index,
+        len = req.query.length,
+        data = []
+    for (var i = 0; i < len; i++) {
+        data.push("内容" + (parseInt(nextIndex) + i))
+    }
+    setTimeout(function () {
+        res.send(data)
+    }, 2000)
+})
+
