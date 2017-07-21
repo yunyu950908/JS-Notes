a35050473296cd2f72fdb69811286eb3b9a90332
yunyu950908 <yunyu950908@gmail.com>
2017-07-21 17:47:20 +0800

    JSONP-CORS-降域

---
+++ advance/task13/.router.js
@@ -0,0 +1,38 @@
+function setRouter(app){ 
+ var router = app; 
+
+router.get('/getNews', function (req, res) {
+    var news = [
+        "111111",
+        "222222",
+        "333333",
+        "444444",
+        "555555",
+        "666666"
+    ];
+    var data = []
+    for (var i = 0; i < 3; i++) {
+        var index = parseInt(Math.random() * news.length);
+        data.push(news[index]);
+        news.splice(index, 1);
+    }
+
+
+    //CORS
+
+    // res.header("Access-Control-Allow-Origin", "*")
+    // res.send(data)
+    // console.log(data)
+
+
+    //JSONP
+
+    // var callback = req.query.callback;
+    // if (callback) {
+    //     res.send(callback + '(' + JSON.stringify(data) + ')');
+    // } else {
+    //     res.send(data);
+    // }
+})
+}
+ module.exports.setRouter = setRouter
