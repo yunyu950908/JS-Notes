e828dbd334a3d7a1925fac6e1e3df7a2fc7b6b24
yunyu950908 <yunyu950908@gmail.com>
2017-07-24 01:26:19 +0800

    jq-ajax

--- .router.js
+++ .router.js
@@ -1,17 +1,17 @@
 function setRouter(app){ 
  var router = app; 
 
-router.get('/getInfo', function (req, res) {
+router.get('/loadMore', function (req, res) {
     var nextIndex = req.query.index,
         len = req.query.length,
         data = []
+    console.log(nextIndex,len)
     for (var i = 0; i < len; i++) {
-        data.push("内容" + (parseInt(nextIndex) + i))
+        data.push("JOJO的奇妙冒险" + (parseInt(nextIndex) + i))
     }
     setTimeout(function () {
         res.send(data)
-    }, 1000)
+    }, 300)
 })
-
 }
  module.exports.setRouter = setRouter
--- advance/task15/router.js
+++ advance/task15/router.js
@@ -1,12 +1,12 @@
-router.get('/getInfo', function (req, res) {
+router.get('/loadMore', function (req, res) {
     var nextIndex = req.query.index,
         len = req.query.length,
         data = []
+    console.log(nextIndex,len)
     for (var i = 0; i < len; i++) {
-        data.push("内容" + (parseInt(nextIndex) + i))
+        data.push("JOJO的奇妙冒险" + (parseInt(nextIndex) + i))
     }
     setTimeout(function () {
         res.send(data)
-    }, 1000)
+    }, 300)
 })
-
