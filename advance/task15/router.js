51abb37c8d1b0a1483b4f8eea9d441bc60016d18
yunyu950908 <yunyu950908@gmail.com>
2017-07-24 02:06:37 +0800

    modify

--- advance/task15/router.js
+++ advance/task15/router.js
@@ -2,10 +2,11 @@ router.get('/loadMore', function (req, res) {
     var nextIndex = req.query.index,
         len = req.query.length,
         data = []
-    console.log(nextIndex,len)
+    console.log(nextIndex, len)
     for (var i = 0; i < len; i++) {
         data.push("JOJO的奇妙冒险" + (parseInt(nextIndex) + i))
     }
+    res.header("Access-Control-Allow-Origin", "*")
     setTimeout(function () {
         res.send(data)
     }, 300)
